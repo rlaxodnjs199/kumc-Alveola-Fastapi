f14a7a95d2a748d0993a2143271b947ad3339ac1
rlaxodnjs199 <rlaxodnjs199@gmail.com>
2023-04-18 12:12:10 -0500

    Remove unnecessary coments

--- app/project/service.py
+++ app/project/service.py
@@ -17,7 +17,6 @@ class ProjectService:
     async def get_all_projects(
         *, db_session: AsyncSession
     ) -> List[Optional[Project]]:
-        """Returns all projects"""
         q = await db_session.execute(select(Project))
         await db_session.commit()
         return q.scalars().all()
@@ -26,7 +25,6 @@ class ProjectService:
     async def get_project(
         *, db_session: AsyncSession, project_id: int
     ) -> Optional[Project]:
-        """Returns a project based on the project_id"""
         project = await db_session.get(Project, project_id)
         await db_session.commit()
         return project
@@ -35,7 +33,6 @@ class ProjectService:
     async def get_project_by_name(
         *, db_session: AsyncSession, project_name: str
     ) -> Optional[Project]:
-        """Returns a project based on the project_name"""
         q = await db_session.execute(
             select(Project).where(Project.name == project_name)
         )
@@ -46,7 +43,6 @@ class ProjectService:
     async def create_project(
         *, db_session: AsyncSession, project_in: ProjectCreate
     ):
-        """Creates a project"""
         stmt = insert(Project).values(**project_in.dict())
         await db_session.execute(stmt)
         await db_session.commit()
@@ -58,7 +54,6 @@ class ProjectService:
         project: Project,
         project_in: ProjectUpdate,
     ):
-        """Updates a project"""
         project_data = ProjectUpdate.from_orm(project).dict()
         to_update = {
             k: v for k, v in project_in.dict().items() if v is not None
@@ -75,7 +70,6 @@ class ProjectService:
 
     @staticmethod
     async def delete_project(*, db_session: AsyncSession, project_id: int):
-        """Deletes a project"""
         stmt = delete(Project).where(Project.id == project_id)
         await db_session.execute(stmt)
         await db_session.commit()
